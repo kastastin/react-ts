3edf41c90eba6c580b6407eceec1ac5c9d5c011a
kastastin <kostya242421@gmail.com>
2024-02-02 15:58:51 +0200

    initial commit

---
+++ first-app.ts
@@ -0,0 +1,13 @@
+let userName = 'kastastin';
+
+// userName = 10; // Type 'number' is not assignable to type 'string'
+userName = 'bob';
+
+// string,number, boolean
+let userAge = 20;
+let isValid = true;
+
+// type string OR number
+let userId: string | number = 'abc1';
+userId = 123;
+// userId = true; // Type 'boolean' is not assignable to type 'string | number'
