56bd1c8ac566b1e2254de973eef8183f48ed6e6c
kastastin <kostya242421@gmail.com>
2024-02-04 16:31:19 +0200

    useRef, type-safe way handling user input

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -14,12 +14,12 @@ export type CourseGoal = {
 export default function App() {
 	const [goals, setGoals] = useState<CourseGoal[]>([]);
 
-	function handleAddGoal() {
+	function handleAddGoal(goal: string, summary: string) {
 		setGoals((prevGoals) => {
 			const newGoal: CourseGoal = {
 				id: Math.random(),
-				title: 'Learn React + TS',
-				description: 'Learn it in depth',
+				title: goal,
+				description: summary,
 			};
 
 			return [...prevGoals, newGoal];
@@ -36,7 +36,7 @@ export default function App() {
 				<h1>Your course goals</h1>
 			</Header>
 
-			<NewGoal />
+			<NewGoal onAddGoal={handleAddGoal} />
 
 			<CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
 		</main>
