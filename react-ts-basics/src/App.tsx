10be059c690d3db970ec876e1fafed17b76a9355
kastastin <kostya242421@gmail.com>
2024-02-04 02:56:19 +0200

    useState, state, outputting state-based values

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -1,18 +1,47 @@
+import { useState } from 'react';
+
 import CourseGoal from './components/CourseGoal.tsx';
 import Header from './components/Header.tsx';
-
 import goalsImg from './assets/goals.jpg';
 
+type CourseGoal = {
+	id: number;
+	title: string;
+	description: string;
+};
+
 export default function App() {
+	const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+	function handleAddGoal() {
+		setGoals((prevGoals) => {
+			const newGoal: CourseGoal = {
+				id: Math.random(),
+				title: 'Learn React + TS',
+				description: 'Learn it in depth',
+			};
+
+			return [...prevGoals, newGoal];
+		});
+	}
+
 	return (
 		<main>
 			<Header image={{ src: goalsImg, alt: 'A list of goals' }}>
 				<h1>Your course goals</h1>
 			</Header>
 
-			<CourseGoal title='Learn React + TS'>
-				<p>Learn it from the ground up</p>
-			</CourseGoal>
+			<button onClick={handleAddGoal}>Add Goal</button>
+
+			<ul>
+				{goals.map((goal) => (
+					<li key={goal.id}>
+						<CourseGoal title={goal.title}>
+							<p>{goal.description}</p>
+						</CourseGoal>
+					</li>
+				))}
+			</ul>
 		</main>
 	);
 }
