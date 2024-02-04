b1b16f5038f86188dcdb8a4234524395810ab59e
kastastin <kostya242421@gmail.com>
2024-02-04 23:58:11 +0200

    better polymorphic component with generics

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,7 +4,13 @@ import Container from './components/Container.tsx';
 function App() {
 	return (
 		<main>
-			<Container as={Button} />
+			<Container
+				as={Button}
+				type='button'
+				onClick={() => console.log('clicked')}
+			>
+				Click me
+			</Container>
 		</main>
 	);
 }
--- example/src/components/Container.tsx
+++ example/src/components/Container.tsx
@@ -1,9 +1,24 @@
-import { type ElementType } from 'react';
+import {
+	type ReactNode,
+	type ElementType,
+	type ComponentPropsWithoutRef,
+} from 'react';
 
-type ContainerProps = {
-	as: ElementType;
-};
+type ContainerProps<T extends ElementType> = {
+	as?: T;
+	children: ReactNode;
+} & ComponentPropsWithoutRef<T>;
 
-export default function Container({ as: Component }: ContainerProps) {
-	return <Component />;
+export default function Container<C extends ElementType>({
+	as,
+	children,
+	...props
+}: ContainerProps<C>) {
+	const Component = as || 'div';
+
+	return (
+		<Component className='container' {...props}>
+			{children}
+		</Component>
+	);
 }
