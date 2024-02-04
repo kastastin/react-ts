fd25e0fb68b74a8080e25ee0e6939fe6f05724a0
kastastin <kostya242421@gmail.com>
2024-02-04 03:07:44 +0200

    reusing types across files

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
-import CourseGoal from './components/CourseGoal.tsx';
 import Header from './components/Header.tsx';
+import CourseGoalList from './components/CourseGoalList.tsx';
 import goalsImg from './assets/goals.jpg';
 
 type CourseGoal = {
@@ -33,15 +33,7 @@ export default function App() {
 
 			<button onClick={handleAddGoal}>Add Goal</button>
 
-			<ul>
-				{goals.map((goal) => (
-					<li key={goal.id}>
-						<CourseGoal title={goal.title}>
-							<p>{goal.description}</p>
-						</CourseGoal>
-					</li>
-				))}
-			</ul>
+			<CourseGoalList goals={goals} />
 		</main>
 	);
 }
