136c6e914a1d022e2cbc8b5d9d7b60a2a5d8fe00
kastastin <kostya242421@gmail.com>
2024-02-05 21:14:10 +0200

    fetching + transforming data

--- data-fetch/src/App.tsx
+++ data-fetch/src/App.tsx
@@ -1,5 +1,52 @@
+import { useState, useEffect, type ReactNode } from 'react';
+
+import BlogPosts, { type BlogPost } from './components/BlogPosts.tsx';
+import { get } from './util/http.ts';
+
+import fetchingImg from './assets/data-fetching.png';
+
+type RawDataBlogPost = {
+	id: number;
+	userId: number;
+	title: string;
+	body: string;
+};
+
 function App() {
-	return <h1>Data Fetching!</h1>;
+	const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+
+	useEffect(() => {
+		async function fetchPosts() {
+			const data = (await get(
+				'https://jsonplaceholder.typicode.com/posts'
+			)) as RawDataBlogPost[];
+
+			const blogPosts: BlogPost[] = data.map((rawPost) => {
+				return {
+					id: rawPost.id,
+					title: rawPost.title,
+					text: rawPost.body,
+				};
+			});
+
+			setFetchedPosts(blogPosts);
+		}
+
+		fetchPosts();
+	}, []);
+
+	let content: ReactNode;
+
+	if (fetchedPosts) {
+		content = <BlogPosts posts={fetchedPosts} />;
+	}
+
+	return (
+		<main>
+			<img src={fetchingImg} alt='Fetching Image' />
+			{content}
+		</main>
+	);
 }
 
 export default App;
